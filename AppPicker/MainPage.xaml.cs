93b14fec39e4f3b3e59bb3c67ef9b30afcbd20b1
felipricardo <felipebarbosaok@gmail.com>
2023-09-21 16:49:22 -0300

    Add project files.

---
+++ AppPicker/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppPicker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public Boolean NumeroPar(int valor)
+        {
+            Boolean retorno = false;
+            if (valor % 2 == 0) { retorno = true; }
+            return retorno;
+        }
+
+        public int CalculaFatorial(int valor)
+        {
+            int retorno = 0;
+            //implementar: validar entradas negativas
+            if (valor == 0)
+            {
+                retorno = 1;
+            }
+            else
+            {
+                retorno = valor;
+                for (int i = valor - 1; i > 0; i--)
+                {
+                    retorno = retorno * i;
+                }
+            }
+            return retorno;
+        }
+
+        public Boolean NumeroPrimo(int valor)
+        {
+            Boolean retorno = false;
+            int count = 0;
+            for (int i = valor; i > 0; i--)
+            {
+                if (valor % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        private void btVerificar_Clicked(object sender, EventArgs e)
+        {
+            int valor = Convert.ToInt32(etValor.Text);
+            String iPicker = (String) pkOperacao.SelectedItem;
+            if(iPicker == "Numero primo")
+            {
+                String resposta = "Não é um numero primo";
+
+                if (NumeroPrimo() == true)
+                {
+                    resposta = "É um numero primo";
+                }
+            }
+        }
+    }
+}
